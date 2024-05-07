4b6cd92426a5f56263c1e5f755b1a469f4fa1968
Agastya Hajela <169131312+Agastya-Hajela@users.noreply.github.com>
2024-05-07 12:38:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,119 @@
+document.getElementById("a01"); 
+
+function playAudio01() { 
+  a01.play(); 
+}
+
+document.getElementById("a02"); 
+
+function playAudio02() { 
+  a02.play(); 
+}
+
+document.getElementById("a03"); 
+
+function playAudio03() { 
+  a03.play(); 
+}
+
+document.getElementById("a04"); 
+
+function playAudio04() { 
+  a04.play(); 
+}
+
+document.getElementById("a05"); 
+
+function playAudio05() { 
+  a05.play(); 
+}
+
+document.getElementById("a06"); 
+
+function playAudio06() { 
+  a06.play(); 
+}
+
+document.getElementById("a07"); 
+
+function playAudio07() { 
+  a07.play(); 
+}
+
+document.getElementById("a08"); 
+
+function playAudio08() { 
+  a08.play(); 
+}
+
+document.getElementById("a09"); 
+
+function playAudio09() { 
+  a09.play(); 
+}
+
+document.getElementById("a10"); 
+
+function playAudio10() { 
+  a10.play(); 
+}
+
+document.getElementById("a11"); 
+
+function playAudio11() { 
+  a11.play(); 
+}
+
+document.getElementById("a12"); 
+
+function playAudio12() { 
+  a12.play(); 
+}
+
+document.getElementById("a13"); 
+
+function playAudio13() { 
+  a13.play(); 
+}
+
+document.getElementById("a14"); 
+
+function playAudio14() { 
+  a14.play(); 
+}
+
+document.getElementById("a15"); 
+
+function playAudio15() { 
+  a15.play(); 
+}
+
+document.getElementById("a16"); 
+
+function playAudio16() { 
+  a16.play(); 
+}
+
+document.getElementById("a17"); 
+
+function playAudio17() { 
+  a17.play(); 
+}
+
+document.getElementById("a18"); 
+
+function playAudio18() { 
+  a18.play(); 
+}
+
+document.getElementById("a19"); 
+
+function playAudio19() { 
+  a19.play(); 
+}
+
+document.getElementById("a20"); 
+
+function playAudio20() { 
+  a20.play(); 
+}
